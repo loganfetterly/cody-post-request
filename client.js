ed74ad471960cf4bf615900d12058fee9c9883fc
Logan Fetterly <lfetterly25@gmail.com>
2020-08-03 17:35:07 -0600

    Working solution of POST request

---
+++ client.js
@@ -0,0 +1,27 @@
+const fetch = require('node-fetch');
+
+const postData = async ( url = '', data = {})=>{
+    // console.log(data);
+
+      const response = await fetch(url, {
+
+      method: 'POST', 
+      credentials: 'same-origin',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+     // Body data type must match "Content-Type" header        
+      body: JSON.stringify(data), 
+    });
+
+      try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+      }catch(error) {
+      console.log("error", error);
+      }
+  }
+
+postData('http://localhost:8000/addMovie', {movie:' the matrix', score: 5});
+    
